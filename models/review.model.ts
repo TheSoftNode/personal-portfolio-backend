c61d9f6cce78259c7751b1c3532c00082c51a1a2
TheSoftNode <thesoftnode@gmail.com>
2024-09-27 20:51:54 +0100

    Add review model

---
+++ models/review.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { IReview } from "../Interfaces/review.interface";
+
+
+const reviewSchema = new Schema<IReview>(
+    {
+        userFullname: {
+            type: String,
+            required: [true, "Please provide your full name"],
+            unique: true
+        },
+
+        userTitle: {
+            type: String,
+            required: [true, "Please enter your title"],
+            unique: true
+        },
+
+        reviewText: {
+            type: String,
+            required: [true, "Review can not be empty!"],
+        },
+
+        reviewRating: {
+            type: Number,
+            min: 1,
+            max: 5,
+        },
+
+        userPhoto: {
+            type: String,
+            default: "default.jpg"
+        },
+
+        userLinks: [{
+            title: { type: String, required: true },
+            link: { type: String, required: true }
+        }]
+    },
+    { timestamps: true }
+);
+
+
+const Review: Model<IReview> = mongoose.model(
+    "Review",
+    reviewSchema
+);
+export default Review;
